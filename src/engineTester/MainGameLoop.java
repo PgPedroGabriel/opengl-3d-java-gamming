4acd3afa02eadaa6111600af0c1f69936a9187ff
pg <pedrogabriel.17@hotmail.com>
2015-11-14 12:36:12 -0300

    First commit with render a cubo rotating

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,134 @@
+package engineTester;
+
+import javax.swing.Renderer;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+
+import entities.Camera;
+import entities.Entity;
+import models.RawModel;
+import models.TextureModel;
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.Render;
+import shaders.StaticShader;
+import textures.ModelTexture;
+
+public class MainGameLoop {
+
+	public static void main(String[] args) {
+		
+		DisplayManager.createDisplay();
+		
+		Loader loader = new Loader();
+		StaticShader shader = new StaticShader();
+		Render renderer = new Render(shader);
+		
+		float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
+				
+		};
+		
+		float[] textureCoords = {
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
+		};
+		
+		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+			
+		TextureModel staticModel = new TextureModel(model, new ModelTexture(loader.loadTexture("e")));
+		
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-5), 0,0,0, 1);
+		
+		Camera camera = new Camera();
+		
+		while(!Display.isCloseRequested())
+		{
+			entity.increaseRotation(1, 1, 0);
+			camera.move();
+			renderer.prepare();
+			shader.start();
+			shader.loadViewMatrix(camera);
+			renderer.render(entity, shader);
+			shader.stop();
+			DisplayManager.updatedisplay();
+		
+		}
+
+		shader.cleanUp();
+		loader.cleanUp();
+		
+		DisplayManager.closeDisplay();
+	}
+
+}
